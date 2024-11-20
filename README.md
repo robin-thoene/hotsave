1ecf0a932710774269e86c7ab3215a5846591f58
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-11-20 23:11:02 +0100

    enable setting save and restore key with toml file

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 Immediately backup and restore a backup for a single file using global hotkeys.
 
+## Purpose
+
+An example use case for me personally is saving current game save states to restore them on a key press later. This is exceptionally useful
+for challenging games, like the Souls series, where on death you would have to redo a time intensive and cumbersome running section to reach a
+boss again.
+
+While there are already some tools out there, I decided to do this application for myself for the following reasons:
+
+- trying out cli related Rust crates
+- implementing a CI/CD process for a compiled binary as learning
+- concentrate on core functionality instead of providing additional features like save management
+- no GUI
+- fun
+
 ## Requirements
 
 ### Linux
@@ -23,3 +37,16 @@ cargo install --git https://github.com/robin-thoene/hotsave
 ### Prebuilt
 
 To get a prebuilt binary check out the [release page](https://github.com/robin-thoene/hotsave/releases) and download it there.
+
+## Customization
+
+You can customize the application behavior by creating a config file located at **~/.config/hotsave/hotsave.toml** with the contents
+(note that the values are examples):
+
+```shell
+save_file_key = "F7"
+restore_file_key = "F8"
+```
+
+If you are unsure what the name of the key is you want to customize, start the app with the environment variable **RUST_LOG=debug**.
+Just press the desired key, the debug log will show you the **KeyPress** value of that key that you need to use in the config.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
+use config::{Config, File};
 use env_logger::Env;
 use log::{debug, error, info};
 use rdev::{grab, Event, EventType, Key};
-use std::{fmt::Debug, fs::copy, path::PathBuf, process::exit};
+use serde::{self, Deserialize};
+use std::{env, fmt::Debug, fs::copy, path::PathBuf, process::exit};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,9 +17,34 @@ struct Args {
     path: PathBuf,
 }
 
+#[derive(Deserialize, Debug)]
+struct AppConfig {
+    save_file_key: Key,
+    restore_file_key: Key,
+}
+
 fn main() {
     // Setup
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Parse possibly existing config
+    let home = env::var("HOME");
+    let mut config_builder = Config::builder()
+        .set_default("save_file_key", "F4")
+        .expect("The default save file key must be set")
+        .set_default("restore_file_key", "F5")
+        .expect("The default restore file key must be set");
+    if let Ok(home) = home {
+        config_builder = config_builder.add_source(
+            File::with_name(&format!("{}/.config/hotsave/hotsave.toml", home)).required(false),
+        );
+    }
+    let config = config_builder
+        .build()
+        .expect("Expect a configuration, at least initialized with defaults")
+        .try_deserialize::<AppConfig>()
+        .expect("At least the defaul config should be deserializable");
+    debug!("Loaded config: {:#?}", config);
+    // Check cli arguments
     let args = Args::parse();
     debug!(
         "File path \"{}\" was provided as argument",
@@ -45,19 +72,23 @@ fn main() {
         file_name.to_string_lossy(),
         file_extension.to_string_lossy()
     ));
-    let save_file_key = Key::F4;
-    let restore_file_key = Key::F5;
-    info!("Key to save current file version: {:#?}", save_file_key);
-    info!("Key to restore saved file version: {:#?}", restore_file_key);
+    info!(
+        "Key to save current file version: {:#?}",
+        config.save_file_key
+    );
+    info!(
+        "Key to restore saved file version: {:#?}",
+        config.restore_file_key
+    );
     // Build the callback for the key listener
     let grab_callback = move |event: Event| -> Option<Event> {
         match event.event_type {
-            EventType::KeyPress(k) if k == save_file_key => {
+            EventType::KeyPress(k) if k == config.save_file_key => {
                 info!("Saving current file version");
                 _ = copy(&file_path, &save_file_path);
                 None
             }
-            EventType::KeyPress(k) if k == restore_file_key => {
+            EventType::KeyPress(k) if k == config.restore_file_key => {
                 info!("Restore saved file version");
                 _ = copy(&save_file_path, &file_path);
                 None
